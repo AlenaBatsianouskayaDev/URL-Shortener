6c4f67e5298790002692295d9224e4be464ab50e
AlenaBo <95024837+AlenaBatsianouskayaDev@users.noreply.github.com>
2022-01-20 18:35:53 +0200

    added express

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//Install express server
+//Install express server    
 const express = require('express');
-const path = require('path');
 
-const app = express();
+const path = require('path');   
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/url-shortener'));
+const app = express();   
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/url-shortener/index.html'));
-});
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
