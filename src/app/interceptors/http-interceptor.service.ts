c2fc78875177be817706391da29ac2c98219552e
AlenaBo <95024837+AlenaBatsianouskayaDev@users.noreply.github.com>
2022-01-20 14:04:06 +0200

    added requests from front to back

--- src/app/interceptors/http-interceptor.service.ts
+++ src/app/interceptors/http-interceptor.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
 @Injectable()
 export class HttpInterceptorService implements HttpInterceptor{
 
   constructor(private injector: Injector ) { }
 
-  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
+  public intercept(req: HttpRequest<string>, next: HttpHandler): Observable<HttpEvent<any>> {
     let updateReq = req.clone({
       setHeaders: {
         'Content-Type': 'application/json; charset=UTF-8'
